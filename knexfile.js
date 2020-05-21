81c3428ebc13eec343ceb89754ee052d6d40ffce
Jana Scheuble <janascheuble@Janas-MacBook-Air.local>
2020-05-21 10:41:18 -0700

    test passing

--- knexfile.js
+++ knexfile.js
@@ -17,6 +17,21 @@ module.exports = {
     }
   },
 
+  testing: {
+    client: 'pg',
+    connection: {
+      database: 'pgtesting',
+      user: process.env.USERNAME,
+      password: process.env.PASSWORD
+    },
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    }
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DB_URL,
